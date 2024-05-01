f98d68f42b9a42f6dc6f18db4f5cd125e20a10bb
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-05-01 09:12:04 +0300

    test: :test_tube: Create test for catgeory entity.

---
+++ test/unit/domain/category/entity/category_entity_test.dart
@@ -0,0 +1,61 @@
+import 'package:flash_focus/src/core/entity/entity.dart';
+import 'package:flash_focus/src/core/identifier/identifier.dart';
+import 'package:flash_focus/src/domain/category/entity/category_entity.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("CategoryEntity", () {
+    test('should extends Entity', () {
+      final category = CategoryEntity(
+        id: const IntIdentifier(value: 1),
+        name: 'Category 1',
+      );
+
+      expect(category, isA<Entity>());
+    });
+
+    test('should return true when the two categories are equal', () {
+      final category1 = CategoryEntity(
+        id: const IntIdentifier(value: 1),
+        name: 'Category 1',
+      );
+      final category2 = CategoryEntity(
+        id: const IntIdentifier(value: 1),
+        name: 'Category 1',
+      );
+      expect(category1.hashCode, category2.hashCode);
+    });
+
+    test('should return false when the two categories are different', () {
+      final category1 = CategoryEntity(
+        id: const IntIdentifier(value: 1),
+        name: 'Category 1',
+      );
+      final category2 = CategoryEntity(
+        id: const IntIdentifier(value: 2),
+        name: 'Category 2',
+      );
+      expect(category1.hashCode, isNot(category2.hashCode));
+    });
+
+    test('should return the category name', () {
+      final category = CategoryEntity(
+        id: const IntIdentifier(value: 1),
+        name: 'Category 1',
+      );
+
+      expect(category.name, 'Category 1');
+    });
+
+    test('when edit category should return the new name', () {
+      final category = CategoryEntity(
+        id: const IntIdentifier(value: 1),
+        name: 'Category 1',
+      );
+
+      category.edit(name: 'Category 2');
+
+      expect(category.name, 'Category 2');
+    });
+  });
+}
